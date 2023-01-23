c0c4ce3f1ab53b1918b22aa6e43e5d5ce9a6b3a4
Ali <laymoth@pm.me>
2023-01-23 08:32:16 +0700

    Initial

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sint molestiae ipsa, eligendi, voluptas amet dicta eius in aspernatur aliquam saepe. Facilis in quasi porro odio voluptate dicta ullam animi.
+	</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ footer.php
@@ -0,0 +1,9 @@
+
+	<footer style="border-top: 4px solid #ff8680;  ">
+		<div class="copy" style="text-align: center;">
+			<span>Copyright &copy; 2022 Ali.</span>
+		</div>
+	</footer>
+
+</body>
+</html>
